7519618b2f977a8f1273ac8331eaa8f126eab74d
Thompson Edolo <verygreenboi@live.com>
2015-07-04 17:31:06 +0100

    Updated doc

--- lib/binlist.rb
+++ lib/binlist.rb
@@ -5,6 +5,10 @@ module Binlist
 	include HTTParty
 	base_uri "binlist.net" 
 
+	# This is called to find a Bin  
+	# @params bin [string]
+	# @return [Net::HTTPResponse] A subclass of Net::HTTPResponse, e.g. Net::HTTPOK 
+
 	def self.find(bin)
 		get("/json/#{bin}")
 	end
